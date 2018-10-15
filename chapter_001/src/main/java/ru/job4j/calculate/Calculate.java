7172908b3fd61eec19a7bcb4e9c1e0ef31b50cbc
tumen.garmazhapov <gtb-85@yandex.ru>
2018-10-15 16:36:38 +0300

    1.3. JavDoc. Code convetion

--- chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/calculate/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j.calculate;
 
+/**
+* Calculate
+*
+* @author tumen.garmazhapov (gtb-85@yandex.ru)
+*/
+
 public class Calculate {
 	
+	/**
+	* Вывод строки в консоль
+	*main.
+	*@param args - args
+	*/
 	public static void main(String[] args) {
 		System.out.println("Hello World");
 	}
---
+++ chapter_001/src/main/java/ru/job4j/calculate/package-info.java
@@ -0,0 +1,8 @@
+/**
+* Package for calculate task.
+*
+* @author tumen.garmazhapov (gtb-85@yandex.ru)
+* @version $Id$
+* @since 0.1
+*/
+package ru.job4j.calculate;
