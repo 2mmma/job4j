30a8205ffd24effdfd7f5e1a0cb1d9808ddc129c
tumen.garmazhapov <gtb-85@yandex.ru>
2018-10-23 17:41:53 +0300

    4.2. Максимум из двух чисел[#74549]

---
+++ chapter_001/src/main/java/ru/job4j/max/Max.java
@@ -0,0 +1,18 @@
+package ru.job4j.max;
+
+/**
+ * @author tumen.garmazhapov (gtb-85@yandex.ru)
+ * @version $Id$
+ * @since 0.1
+ */
+public class Max {
+    
+	/**
+     * Возвращает максимальное число.
+     * @param first, second два числа.
+     * @return максимальное значение.
+	 */
+	 public int max(int first, int second) {
+		 return first > second ? first : second;
+	 }
+}
