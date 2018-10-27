a2c789758f0440e85f29197195acdc7d40b412ee
tumen.garmazhapov <gtb-85@yandex.ru>
2018-10-27 19:06:40 +0300

    5.5. Рефакторинг кода.[#74554]

--- chapter_001/src/main/java/ru/job4j/loop/Paint.java
+++ chapter_001/src/main/java/ru/job4j/loop/Paint.java
@@ -1,5 +1,6 @@
 package ru.job4j.loop;
 
+import java.util.function.BiPredicate;
 /**
  * Программа рисует пирамиду на псевдографике
  * @author tumen.garmazhapov (gtb-85@yandex.ru)
@@ -11,25 +12,42 @@ public class Paint {
      * @param height высота пирамиды.
      * @return screen.toString();
      */
-    public String pyramid(int height) {
-        // Буфер для результата.
-        StringBuilder screen = new StringBuilder();
-        // ширина будет равна высоте.
-        int weight = 2 * height - 1 ;
-        // внешний цикл двигается по строкам.
-        for (int row = 0; row != height; row++) {
-            // внутренний цикл определяет положение ячейки в строке.
-            for (int column = 0; column != weight; column++) {
-                if (row >= height - column - 1 && row + height - 1 >= column) {
-                    screen.append("^");
-                } else {
-                    screen.append(" ");
-                }
-            }
-            // перевод строки.
-            screen.append(System.lineSeparator());
+    public String rightTrl(int height) {
+            return this.loopBy(
+                    height,
+                    height,
+                    (row, column) -> row >= column
+            );
         }
-        //  результат.
-        return screen.toString();
+
+        public String leftTrl(int height) {
+            return this.loopBy(
+                    height,
+                    height,
+                    (row, column) -> row >= height - column - 1
+            );
+        }
+
+        public String pyramid(int height) {
+            return this.loopBy(
+                    height,
+                    2 * height - 1,
+                    (row, column) -> row >= height - column - 1 && row + height - 1 >= column
+            );
+        }
+
+        private String loopBy(int height, int weight, BiPredicate<Integer, Integer> predict) {
+            StringBuilder screen = new StringBuilder();
+            for (int row = 0; row != height; row++) {
+                for (int column = 0; column != weight; column++) {
+                    if (predict.test(row, column)) {
+                        screen.append("^");
+                    } else {
+                        screen.append(" ");
+                    }
+                }
+                screen.append(System.lineSeparator());
+            }
+            return screen.toString();
     }
 }
