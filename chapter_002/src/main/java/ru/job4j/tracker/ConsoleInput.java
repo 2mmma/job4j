e11fc4d11101c8fad800c37030d5df5360ba8105
tumen.garmazhapov <gtb-85@yandex.ru>
2018-12-19 21:18:02 +0300

    1. Используя класс ConsoleInput в классе StartUI обеспечить полноценную работу всего приложения[#74622]

---
+++ chapter_002/src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -0,0 +1,19 @@
+package ru.job4j.tracker;
+
+import java.util.*;
+
+/**
+ * @version 1.0
+ * @since 12.2018
+ * @author tumen.garmazhapov (gtb-85@yandex.ru)
+ */
+
+public class ConsoleInput implements Input{
+
+    private Scanner scanner = new Scanner(System.in);
+
+    public String ask(String question){
+        System.out.println(question);
+        return scanner.next();
+    }
+}
---
+++ chapter_002/src/main/java/ru/job4j/tracker/Input.java
@@ -0,0 +1,11 @@
+package ru.job4j.tracker;
+
+/**
+ * @version 1.0
+ * @since 12.2018
+ * @author tumen.garmazhapov (gtb-85@yandex.ru)
+ */
+
+public interface Input {
+    String ask(String question);
+}
