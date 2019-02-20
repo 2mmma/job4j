ee251879f87554311fdf9afbc85b73775a93da3e
tumen.garmazhapov <gtb-85@yandex.ru>
2019-02-20 11:56:50 +0300

    2. Написать программу преобразования List в Map.[#74615]

---
+++ chapter_003/src/main/java/ru/job4j/user/User.java
@@ -0,0 +1,27 @@
+package ru.job4j.user;
+
+/**
+ * @author tumen.garmazhapov
+ * @since 02.2019
+ */
+public class User {
+    private Integer id;
+    private String name;
+    private String city;
+
+    public User(Integer id, String name, String city) {
+        this.id = id;
+        this.name = name;
+        this.city = city;
+    }
+
+    public Integer getId() { return this.id; }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getCity() { return this.city; }
+
+
+}
---
+++ chapter_003/src/main/java/ru/job4j/user/UserConvert.java
@@ -0,0 +1,23 @@
+package ru.job4j.user;
+
+import java.util.HashMap;
+import java.util.List;
+
+/**
+ * @author tumen.garmazhapov
+ * @since 02.2019
+ */
+public class UserConvert {
+
+    /**
+     * метод конвертирует список List в интерфейс Map.
+     * @param list - список.
+     * @return userMap.
+     */
+    public HashMap<Integer, User> process(List<User> list) {
+        HashMap<Integer, User> userMap = new HashMap<>();
+        for (User user : list) {
+            userMap.put(user.getId(),user);
+        }return userMap;
+    }
+}
