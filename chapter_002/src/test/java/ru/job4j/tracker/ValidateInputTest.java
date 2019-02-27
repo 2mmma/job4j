f49280382aff35fd2566d8f5966e345643d00625
tumen.garmazhapov <gtb-85@yandex.ru>
2019-02-27 20:18:04 +0300

    исправления ошибок

--- chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
+++ chapter_002/src/test/java/ru/job4j/tracker/ValidateInputTest.java
@@ -7,6 +7,7 @@ import org.junit.Test;
 import java.io.ByteArrayOutputStream;
 import java.io.PrintStream;
 
+import static java.lang.String.format;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
 
@@ -33,7 +34,7 @@ import static org.hamcrest.Matchers.is;
     public void whenInvalidLetterInput() {
         ValidateInput input = new ValidateInput(new StubInput(new String[] {"a", "1"}));
         input.ask("Enter", new int[]{1});
-        assertThat(this.mem.toString(), is(String.format("Некорректный ввод! Введите в числовом формате.\r\n"))
+        assertThat(this.mem.toString(), is(format("Некорректный ввод! Введите в числовом формате.\n"))
         );
     }
 
@@ -41,7 +42,7 @@ import static org.hamcrest.Matchers.is;
     public void whenInvalidArrayOutInput() {
         ValidateInput input = new ValidateInput(new StubInput(new String[] {"9", "1"}));
         input.ask("Enter", new int[]{1});
-        assertThat(this.mem.toString(), is(String.format("Некорректный ввод! Введите число из диапазона меню.\r\n"))
+        assertThat(this.mem.toString(), is(format("Некорректный ввод! Введите число из диапазона меню.\n"))
         );
     }
 }
