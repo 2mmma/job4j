db0bf8c5e31872b31a8a5ab8c6b31fd61e1ddd98
tumen.garmazhapov <gtb-85@yandex.ru>
2019-03-19 20:04:34 +0300

    0. Stream API[#74518] fix

--- chapter_004/src/main/java/ru/job4j/stream/StreamUsage.java
+++ chapter_004/src/main/java/ru/job4j/stream/StreamUsage.java
@@ -22,10 +22,10 @@ public class StreamUsage {
 
         @Override
         public String toString() {
-            return "Task{" +
-                    "name='" + name + '\'' +
-                    ", spent=" + spent +
-                    '}';
+            return "Task{"
+                    + "name='" + name + '\''
+                    + ", spent=" + spent
+                    + '}';
         }
     }
 
