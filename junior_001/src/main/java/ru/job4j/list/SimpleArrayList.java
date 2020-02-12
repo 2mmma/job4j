327873f423e3a3f153b4fc8f6ad82a62bb6ce9ed
tumen.garmazhapov <gtb-85@yandex.ru>
2020-02-12 23:21:31 +0300

    5.3.0 Создать метод delete для односвязного списка[#223851]

---
+++ junior_001/src/main/java/ru/job4j/list/SimpleArrayList.java
@@ -0,0 +1,62 @@
+package ru.job4j.list;
+
+/**
+ * @author tumen.garmazhapov (gtb-85@yandex.ru)
+ * @since 02.2020
+ * Класс SimpleArrayList.
+ */
+public class SimpleArrayList<E> {
+
+    private int size;
+    private Node<E> first;
+
+    /**
+     * Метод вставляет в начало списка данные.
+     */
+    public void add(E data) {
+        Node<E> newLink = new Node<>(data);
+        newLink.next = this.first;
+        this.first = newLink;
+        this.size++;
+    }
+
+    /**
+     * Метод удаления первого элемент в списке.
+     */
+    public E delete() {
+        this.first = this.first.next;
+        size--;
+        return this.first.data;
+    }
+
+    /**
+     * Метод получения элемента по индексу.
+     */
+    public E get(int index) {
+        Node<E> result = this.first;
+        for (int i = 0; i < index; i++) {
+            result = result.next;
+        }
+        return result.data;
+    }
+
+    /**
+     * Метод получения размера коллекции.
+     */
+    public int getSize() {
+        return this.size;
+    }
+
+    /**
+     * Класс предназначен для хранения данных.
+     */
+    private static class Node<E> {
+
+        E data;
+        Node<E> next;
+
+        Node(E data) {
+            this.data = data;
+        }
+    }
+}
