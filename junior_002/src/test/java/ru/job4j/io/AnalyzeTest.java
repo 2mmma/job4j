6cb82c1561bff656ed5e1152132c2366453ef6aa
tumen.garmazhapov <gtb-85@yandex.ru>
2020-06-16 14:23:32 +0300

    2. Тестирование IO [#223888]

--- junior_002/src/test/java/ru/job4j/io/AnalyzeTest.java
+++ junior_002/src/test/java/ru/job4j/io/AnalyzeTest.java
@@ -1,7 +1,9 @@
 package ru.job4j.io;
 
 
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
 
@@ -11,6 +13,9 @@ import static org.hamcrest.Matchers.is;
 
 
 public class AnalyzeTest {
+
+    @Rule
+    public TemporaryFolder folder = new TemporaryFolder();
 
     @Test
     public void whenUnavailableTest() throws IOException {
@@ -23,6 +28,28 @@ public class AnalyzeTest {
             assertThat(reader.readLine(), is("11:01:02;11:02:02"));
             assertThat(reader.readLine(), is("11:05:02;11:15:02"));
             assertNull(reader.readLine());
+        }
+    }
+
+    @Test
+    public void whenTemporaryFilesTest() throws IOException {
+        Analyze analyze = new Analyze();
+        File source = folder.newFile("sourceTemporary.txt");
+        File target = folder.newFile("targetTemporary.txt");
+        try (FileWriter writer = new FileWriter(source.getAbsolutePath())) {
+            String text = "server.log\n"
+                    + "200 10:56:01\n"
+                    + "200 10:57:01\n"
+                    + "400 10:58:01\n"
+                    + "200 10:59:01";
+            writer.write(text);
+            writer.append("\n");
+            writer.append("E");
+            writer.flush();
+            analyze.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
+            try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
+                assertThat(reader.readLine(), is("10:58:01;10:59:01"));
+            }
         }
     }
 }
