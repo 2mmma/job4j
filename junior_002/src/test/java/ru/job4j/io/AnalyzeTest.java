8df5eb3ad2193a63386c8dd6323f86d7c8dbddd8
tumen.garmazhapov <gtb-85@yandex.ru>
2020-06-17 13:34:08 +0300

    2. Тестирование IO [#223888] fix test

--- junior_002/src/test/java/ru/job4j/io/AnalyzeTest.java
+++ junior_002/src/test/java/ru/job4j/io/AnalyzeTest.java
@@ -1,11 +1,13 @@
 package ru.job4j.io;
 
-
 import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.TemporaryFolder;
 
 import java.io.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
@@ -37,12 +39,9 @@ public class AnalyzeTest {
         File source = folder.newFile("sourceTemporary.txt");
         File target = folder.newFile("targetTemporary.txt");
         try (FileWriter writer = new FileWriter(source.getAbsolutePath())) {
-            String text = "server.log\n"
-                    + "200 10:56:01\n"
-                    + "200 10:57:01\n"
-                    + "400 10:58:01\n"
-                    + "200 10:59:01";
-            writer.write(text);
+            List<String> list = Arrays.asList("server.log", "200 10:56:01", "200 10:57:01", "400 10:58:01", "200 10:59:01");
+            String result = list.stream().collect(Collectors.joining(System.lineSeparator()));
+            writer.write(String.valueOf(result));
             writer.flush();
             analyze.unavailable(source.getAbsolutePath(), target.getAbsolutePath());
             try (BufferedReader reader = new BufferedReader(new FileReader(target))) {
